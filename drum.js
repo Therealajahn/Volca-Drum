bbd0e29537571d1f42b1357fe7e2f8952ff25864
Therealajahn <therealajahn@gmail.com>
2020-03-20 15:04:45 -0500

    got a punchy envelope

--- drum.js
+++ drum.js
@@ -9,38 +9,44 @@
 
 class Sound {
     constructor(){
-      this.gain = new Tone.Gain(0.4).toMaster();  
+      this.gain = new Tone.Gain(0.4).toMaster(); 
+      this.attack = 0;
+      this.decay = 1;
+      this.sustain = 0;
+      this.release = 1; 
       this.ampEnv = new Tone.AmplitudeEnvelope({
-        "attack" : 0.01,
-        "release" : 10,
+        "attack" : this.attack,
+        "decay": this.decay,
+        "sustain": this.sustain,
+        "release" : this.release,
         
       }).connect(this.gain);
       
       this.filter = new Tone.Filter({
         type : 'lowpass' ,
-        frequency : 1000 ,
+        frequency : 200 ,
         rolloff : -12 ,
         Q : 5,
         gain : 0
         }).connect(this.ampEnv);
       this.osc = new Tone.Oscillator(440, "sawtooth").connect(this.filter).start();
       this.signal = new Tone.Signal().connect(this.osc.frequency);
-      this.mult = new Tone.Multiply(10).connect(this.osc.frequency);
+      this.mult = new Tone.Multiply(500).connect(this.osc.frequency);
       this.modGain = new Tone.Gain(1).connect(this.mult);
-      this.mod = new  Tone.Oscillator(800, "sine").connect(this.modGain).start();
+      this.mod = new  Tone.Oscillator(1000, "sine").connect(this.modGain).start();
       this.modEnv = new Tone.Envelope({
-        "attack" : 0.01,
-        "decay" : 0.01,
-        "sustain" : 0.05,
-        "release" : 1,
+        "attack" : this.attack,
+        "decay" : this.decay,
+        "sustain" : this.sustain,
+        "release" : this.release,
       }).connect(this.modGain);
       this.pitchEnv = new Tone.FrequencyEnvelope({
-        "attack" : 0.01,
-        "decay" : 0.01,
-        "sustain" : 0.05,
-        "release" : 1,
-        "baseFrequency" : "C1",
-        "octaves": 1,
+        "attack" : this.attack,
+        "decay" : this.decay,
+        "sustain" : this.sustain,
+        "release" : this.release,
+        "baseFrequency" : "100",
+        "octaves": 2,
         "exponent" : 1,
       }).connect(this.osc.frequency); 
       
@@ -48,14 +54,16 @@ class Sound {
     }
 }
 
-let sound = new Sound;
+const sound = new Sound;
 
-sound.ampEnv.triggerAttackRelease();
+const duration = .3;
+sound.ampEnv.triggerAttackRelease(duration);
 sound.ampEnv.attackCurve = "exponential";
 sound.ampEnv.releaseCurve = "exponential";
 sound.pitchEnv.attackCurve = "exponential";
 sound.pitchEnv.releaseCurve = "exponential";
-sound.pitchEnv.triggerAttackRelease();
+sound.pitchEnv.triggerAttackRelease(duration);
+sound.modEnv.triggerAttackRelease(duration);
 
 
 
